f401f533ae5dfee75b1a0a5d3c978a326e299fc4
kohd <shane.farrell@yahoo.ie>
2016-01-04 15:25:26 +0000

    Added family tree knowledge base.

--- cs424Assignment.pl
+++ cs424Assignment.pl
@@ -3,3 +3,25 @@ interleave(Xs, [], Xs).
 interleave([], Ys, Ys).
 interleave([X|Xs], [Y|Ys], [X, Y | Zs]) :- 
     interleave(Xs, Ys, Zs).
+
+male(paddy).
+male(shane).
+male(neil).
+male(paul).
+male(patrick).
+
+female(bridget).
+female(margaret).
+female(kathleen).
+
+parent(paddy, shane).
+parent(bridget, shane).
+
+parent(paul, neil).
+parent(margaret, neil).
+
+parent(patrick, paddy).
+parent(kathleen, paddy).
+
+parent(patrick, paul).
+parent(kathleen, paul).
